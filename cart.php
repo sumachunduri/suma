8d8fde266a76b12f96a961b409e0fd4d15fda0cb
sumachunduri <31964237+sumachunduri@users.noreply.github.com>
2019-01-27 16:31:17 -0500

    Add files via upload

---
+++ cart.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<style>
+body {
+	 background-color:lightblue;
+
+}
+</style>
+</head>
+<body>
+
+<b>Your Orders are </b><br><br>
+
+IPHONE :<?php echo $_GET["phonecount"]; ?><br>
+LAPTOP :<?php echo $_GET["lapcount"]; ?><br>
+TV :<?php echo $_GET["tvcount"]; ?><br>
+
+</body>
+</html>
